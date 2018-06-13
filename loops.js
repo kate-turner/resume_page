9bca686c483cd2b996632bde177a7af461d2778b
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-12 22:56:53 -0600

    Commit 9 - partial completd loops.js file.

---
+++ loops.js
@@ -0,0 +1,13 @@
+// for (let i=0; i <= 99; i++) {
+	// console.log(`${i}`);
+// }
+
+// for (let i=99; i >= 0; i--) {
+	// console.log(i);
+for (let i=0; i <= 98; i++) {
+	console.log(i);
+	if (i%2=0) {
+console.log("even number");
+	}
+	
+}
